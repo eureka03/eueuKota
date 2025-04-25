9fb420f844a0cf19958f4c020d1c4e3860bed009
Eureka Baloyi <eureka.baloyi@liquid.tech>
2025-04-25 16:25:00 +0200

    Changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import './Order.css';
+import './Contact.css';
 
 export default function Order(){
     const [form,setForm] = useState({
