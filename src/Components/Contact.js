33e4e70b1a0726dc3b8a7c66f68fe33f8adec1bb
Eureka Baloyi <eureka.baloyi@liquid.tech>
2025-05-02 10:22:23 +0200

    Changes added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import './Contact.css';
+import Thankyou from './Thankyou';
 
 export default function Order(){
     const [form,setForm] = useState({
@@ -15,31 +16,43 @@ export default function Order(){
 
         });
     };
+    const [submited,setSubmited] = useState(false);
+
     const handleSubmit = (e) =>{
+        setSubmited(true);
         e.preventDefault();
-        alert(`Thank you,${form.name}! Your Message has been recieved.
-            We shall get back to you`);
+        //alert(`Thank you,${form.name}! Your Message has been recieved.
+           // We shall get back to you`);
         console.log(form);
         setForm({name:'',phone:'',kotaType:'',extras:''});
     };
+    if(submited){
+        return(
+            <Thankyou/>
+        )
 
-    return(
-        <section className="page-section">
-            <section className="order-section">
-                
-                <form onSubmit={handleSubmit} className="order-form">
-                    <h2>Contact Us</h2>
-                    <input type="text" name="name" value={form.name} placeholder="Your name" onChange={handleChange} required/>
-                    <input type="tel" name="phone" value={form.phone} placeholder="Phone Number" onChange={handleChange} required/>
+    }else{
+        return(
+            <section className="page-section">
+                <section className="order-section">
                     
-                    <textarea
-                     name="extras"
-                     value={form.extras}
-                     placeholder="How can we assist you?"
-                     onChange={handleChange}/>
-                     <button type="submit">Submit</button>
-                </form>
+                    <form onSubmit={handleSubmit} className="order-form">
+                        <h2>Contact Us</h2>
+                        <input type="text" name="name" value={form.name} placeholder="Your name" onChange={handleChange} required/>
+                        <input type="tel" name="phone" value={form.phone} placeholder="Phone Number" onChange={handleChange} required/>
+                        
+                        <textarea
+                         name="extras"
+                         value={form.extras}
+                         placeholder="How can we assist you?"
+                         onChange={handleChange}/>
+                         <button type="submit">Submit</button>
+                    </form>
+                </section>
             </section>
-        </section>
-    )
+        )
+    }
+
+
+   
 }
---
+++ src/Components/Thankyou.js
@@ -0,0 +1,10 @@
+import './Thankyou.css';
+
+export default function Thankyou(){
+    return (
+        <div className="Thankyou">
+            <h2>Your Query has beeen Submitted. We will get back to you</h2>
+        
+        </div>
+    )
+}
