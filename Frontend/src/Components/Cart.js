ddfe23bd71ab1fabefdd206ac0b02558ce88a593
Eureka Baloyi <eurekaripfumelo@gmail.com>
2025-07-14 15:25:08 +0200

    changes added

--- Frontend/src/Components/Cart.js
+++ Frontend/src/Components/Cart.js
@@ -11,7 +11,7 @@ export default function Cart({items,setItems}){
                     <div key={item.id} className="cart-item">
                         <img src={item.src} alt={item.name} />
                         <div className="cart-item-details">
-                            <h2>Name:{item.name}</h2>
+                            <h2>{item.name}</h2>
                             <p>Price: R{item.price}</p>
                             <p>Quantity: {item.quantity}</p>
                         </div>
