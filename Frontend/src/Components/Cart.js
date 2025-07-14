54d0b35e76f36fd660aed5a99de6733ec1870d57
Eureka Baloyi <eurekaripfumelo@gmail.com>
2025-07-14 16:30:24 +0200

    Changes added

--- Frontend/src/Components/Cart.js
+++ Frontend/src/Components/Cart.js
@@ -4,18 +4,49 @@ export default function Cart({items,setItems}){
     return(
         <div className="Cart">
             <h1>Your Cart({length} items)</h1>
+            <div className='cart-header'>
+                <div className='item'>
+                    <p>Item</p>
+                </div>
+                <div className='price'>
+                    <p>Price</p>
+                </div>
+                <div className='Quantity'>
+                    <p>Quantity</p>
+                </div>
+                <div className='total'>
+                    <p>Total</p>
+                </div>
+            </div>
+            
+                
             {items.length === 0 ? (
                 <p class="cart-message">Your cart is empty</p>
             ) : (
                 items.map((item) => (
-                    <div key={item.id} className="cart-item">
-                        <img src={item.src} alt={item.name} />
-                        <div className="cart-item-details">
-                            <h2>{item.name}</h2>
-                            <p>Price: R{item.price}</p>
-                            <p>Quantity: {item.quantity}</p>
+                    
+                        <div key={item.id} className="cart-item">
+                            
+                            <div className="cart-item-details">
+                                <img src={item.src} alt={item.name} />
+                                <h2>{item.name}</h2>
+                                
+                            </div>
+                            <div className='cart-item-price'>
+                                <p>R{item.price}</p>
+                            </div>
+                            <div className='cart-item-quantity'>
+                                <button>-</button>
+                                <input value= {item.quantity}/>
+                                <button>+</button>
+                            </div>
+                            <div className='cart-item-total'>
+                                <p>R{item.quantity*item.price}</p>
+                            </div>
                         </div>
-                    </div>
+                        
+                    
+                    
                 ))
             )}
         </div>
